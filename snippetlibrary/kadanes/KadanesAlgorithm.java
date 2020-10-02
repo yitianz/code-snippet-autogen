2b4792d3eb3ddf1b4a3db34a1b1f4525d1e9a56c
vinnyputty <vinayputreddi@gmail.com>
2020-10-01 17:43:15 -0700

    Add kadanes package with initial classes and json

---
+++ snippetlibrary/kadanes/KadanesAlgorithm.java
@@ -0,0 +1,19 @@
+package kadanes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class KadanesAlgorithm {
+    public static long kadanes(List<Integer> nums){
+        long loc = 0, glob = Long.MIN_VALUE;
+        for (Integer n : nums) {
+            loc = Math.max(n, loc+n);
+            glob = Math.max(loc, glob);
+        }
+        return glob;
+    }
+
+    public static void main(String[] args) {
+        long maxSubarraySum = kadanes(new ArrayList<>());
+    }
+}
---
+++ snippetlibrary/kadanes/KadanesAlgorithmSnippet.java
@@ -0,0 +1,47 @@
+package kadanes;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class KadanesAlgorithmSnippet {
+
+    static final String kadanesInfoFilePath = "snippetlibrary/kadanes/KadanesAlgorithm.json";
+
+    public static String kadanesSnippet(String[] vars, boolean inlineSnippet) throws IOException {
+        // vars should contain the variable names for `localMax` and `globalMax`, in that order
+        // null or empty string for default variable names
+        String returnType = inlineSnippet ? "body" : "method";
+
+//        System.out.println(System.getProperty("user.dir"));
+
+//        BufferedReader jsonFile = new BufferedReader(new FileReader(kadanesInfoFilePath));
+//        jsonFile.lines().forEach(System.out::println);
+
+        String jsonFile = Files.readString(Path.of(kadanesInfoFilePath));
+
+        JSONObject kadanesInfo = new JSONObject(jsonFile).getJSONObject("KadanesAlgorithm");
+        System.out.println(kadanesInfo.toString(4));
+
+        JSONObject extractParams = kadanesInfo.getJSONObject("iterableType").getJSONObject("List<Integer>").getJSONObject(returnType);
+
+        int startLine = extractParams.getInt("startLine") - 1, length = extractParams.getInt("length");
+        StringBuilder sb = new StringBuilder();
+        Files.lines(Path.of(kadanesInfoFilePath.substring(0, kadanesInfoFilePath.lastIndexOf('/') + 1) + kadanesInfo.getString("filename"))).skip(startLine).limit(length).forEach(s -> sb.append(s).append('\n'));
+
+        return sb.toString();
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(kadanesSnippet(new String[]{}, true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
